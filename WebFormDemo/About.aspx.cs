0d66a387c1e06ab61638501368974d347fbaefcf
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-08-19 17:36:05 +0530

    Initial Commit

---
+++ WebFormDemo/About.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormDemo
+{
+    public partial class About : Page
+    {
+        string log = string.Empty;
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            log += "Page OnPreInit";
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            log += "Page OnInit";
+            base.OnInit(e);
+        }
+
+        protected override void OnInitComplete(EventArgs e)
+        {
+            log += "Page OnInitComplete";
+            base.OnInitComplete(e);
+        }
+
+        protected override void OnPreLoad(EventArgs e)
+        {
+            log += "Page OnPreLoad";
+            base.OnPreLoad(e);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            log += "Page Load";
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            log += "Page_LoadComplete";
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            log += "OnPreRender";
+            base.OnPreRender(e);
+        }
+
+        protected override void OnPreRenderComplete(EventArgs e)
+        {
+            log += "OnPreRenderComplete";
+            base.OnPreRenderComplete(e);
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            log += "Page_Unload";
+        }
+    }
+}
---
+++ WebFormDemo/Contact.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" Trace="true" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="WebFormDemo.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your contact page.</h3>
+    <address>
+        One Microsoft Way<br />
+        Redmond, WA 98052-6399<br />
+        <abbr title="Phone">P:</abbr>
+        425.555.0100
+    </address>
+
+    <address>
+        <strong>Support:</strong>   <a href="mailto:Support@example.com">Support@example.com</a><br />
+        <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
+    </address>
+
+
+    <h2>Contact Us</h2>
+
+    <asp:Label ID="lblName" runat="server" Text="Your Name"></asp:Label>
+    <br />
+
+    <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br />
+    <br />
+
+    <asp:Label ID="lblEmail" runat="server" Text="Your Email"></asp:Label><br />
+
+    <asp:TextBox ID="txtEmail" runat="server" TextMode="Email"></asp:TextBox><br />
+    <br />
+
+    <asp:Label ID="lblMessage" runat="server" Text="Your Message"></asp:Label><br />
+
+    <asp:TextBox ID="txtMessage" runat="server" TextMode="MultiLine"></asp:TextBox><br />
+    <br />
+
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+    <br /><br />
+    <asp:Label ID="lblConfirm" runat="server" Text=""></asp:Label>
+
+</asp:Content>
